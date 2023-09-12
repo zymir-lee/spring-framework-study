bb8adf41e7cbb06c24df896b1db342a8afab6dbd
amormz <amormz@foxmail.com>
2023-09-12 21:53:50 +0800

    feat: 拦截器获取controller处理器方法

--- spring-framework-mvc/src/main/java/pers/zymir/spring/mvc/interceptor/WebInterceptor.java
+++ spring-framework-mvc/src/main/java/pers/zymir/spring/mvc/interceptor/WebInterceptor.java
@@ -1,6 +1,7 @@
 package pers.zymir.spring.mvc.interceptor;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -11,8 +12,15 @@ import javax.servlet.http.HttpServletResponse;
 public class WebInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (!(handler instanceof HandlerMethod)) {
+            return true;
+        }
+
+        HandlerMethod handlerMethod = (HandlerMethod) handler;
+        String name = handlerMethod.getMethod().getName();
+
         // boolean值表示是否通过当前拦截器
-        log.info("拦截器pre handle执行");
+        log.info("拦截器pre handle执行, 当前方法：{}", name);
         return true;
     }
 
