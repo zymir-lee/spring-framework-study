444147839d47be62cacd8cbb6428476135d7bed4
amormz <amormz@foxmail.com>
2023-09-12 18:22:09 +0800

    feat: spring mvc基本使用

---
+++ spring-framework-mvc/src/main/java/pers/zymir/spring/mvc/controller/WebController.java
@@ -0,0 +1,50 @@
+package pers.zymir.spring.mvc.controller;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+// 注册为rest controller bean, body通过JSON序列化方式返回
+@RestController
+// URL路径前缀
+@RequestMapping("/web")
+@Slf4j
+public class WebController {
+
+    @GetMapping
+    public String get(@RequestParam("param") String param,
+                      @CookieValue("token") String token,
+                      @RequestHeader("header") String header) {
+        log.info("请求参数获取:{}", param);
+        log.info("http cookie获取:{}", token);
+        log.info("http header获取:{}", header);
+        return "OK";
+    }
+
+    @GetMapping("/{param}")
+    public void pathParam(@PathVariable("param") String param) {
+        log.info("URL路径参数获取:{}", param);
+    }
+
+    @PostMapping
+    public void post(@RequestBody Req req) {
+        log.info("post请求获取body内容: {}", req);
+    }
+
+    @GetMapping
+    public void http(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // 接口获取httpRequest httpResponse对象
+        // request对象用来获取请求报文内容
+        // response对象用来写入响应报文内容 例如状态码等
+    }
+
+    @Data
+    public static class Req {
+        private Long id;
+        private String name;
+    }
+}
