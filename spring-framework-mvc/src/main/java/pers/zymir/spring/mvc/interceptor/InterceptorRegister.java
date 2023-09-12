d78fd08482f221c23b4eaee35b8475e7840461bc
amormz <amormz@foxmail.com>
2023-09-12 21:43:26 +0800

    feat: 拦截器注册细节

--- spring-framework-mvc/src/main/java/pers/zymir/spring/mvc/interceptor/InterceptorRegister.java
+++ spring-framework-mvc/src/main/java/pers/zymir/spring/mvc/interceptor/InterceptorRegister.java
@@ -10,6 +10,8 @@ public class InterceptorRegister implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         WebMvcConfigurer.super.addInterceptors(registry);
         // 注册拦截器，可以设置路径、执行顺序
+        // /*能匹配/web /web1但是不能匹配 /web/demo
+        // /** 可以匹配任意层级路径
         registry.addInterceptor(new WebInterceptor()).addPathPatterns("/**").order(0);
     }
 }
